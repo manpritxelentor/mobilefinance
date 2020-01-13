89a673f46d0a6536eadb905595607dcfd160663b
Manprit <manpritsinghsahota@gmail.com>
2020-01-14 02:04:49 +0530

    Completed implementation of City popup

--- MobileFinanceErp/Controllers/GroupCodeController.cs
+++ MobileFinanceErp/Controllers/GroupCodeController.cs
@@ -1,24 +1,55 @@
 ﻿using MobileFinanceErp.Service;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using MobileFinanceErp.ViewModel;
 using System.Web.Mvc;
 
 namespace MobileFinanceErp.Controllers
 {
-    public class GroupCodeController : Controller
+    public class GroupCodeController : BaseController
     {
         private readonly IGroupCodeService _groupCodeService;
         public GroupCodeController(IGroupCodeService groupCodeService)
         {
             _groupCodeService = groupCodeService;
         }
-        
+
         [HttpPost]
         public ActionResult GetAll(string codeName)
         {
             return Json(_groupCodeService.GetAll(codeName), JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public ActionResult CreateGroupCode(string code)
+        {
+            var model = new AddEditGroupCodeViewModel { Name = code };
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult SaveGroupCode(AddEditGroupCodeViewModel model)
+        {
+            if (!ModelState.IsValid)
+                return Json(new { Status = false, Error = ModelState });
+
+            bool isSuccess = false;
+            if (model.Id > 0)
+            {
+                // Update logic
+                isSuccess = _groupCodeService.Update(model);
+            }
+            else
+            {
+                // Insert logic
+                isSuccess = _groupCodeService.Insert(model);
+            }
+            return Json(new { Status = isSuccess });
+        }
+
+        [HttpPost]
+        public ActionResult GetGroupCodeData(string code, DataSourceRequest dataSourceRequest)
+        {
+            DataSourceResult data = _groupCodeService.GetAll(code, dataSourceRequest);
+            return Json(data, JsonRequestBehavior.AllowGet);
         }
     }
 }
---
+++ MobileFinanceErp/ViewModel/AddEditGroupCodeViewModel.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation.Attributes;
+using MobileFinanceErp.ViewModel.Validators;
+using System.ComponentModel;
+
+namespace MobileFinanceErp.ViewModel
+{
+    [Validator(typeof(AddEditGroupCodeViewModelValidator))]
+    public class AddEditGroupCodeViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        [DisplayName("Name")]
+        public string DisplayName { get; set; }
+    }
+}
