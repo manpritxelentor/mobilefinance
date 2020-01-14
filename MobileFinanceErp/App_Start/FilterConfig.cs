b3641bad2a816ad850e459b5b68e380cfe6c44af
Parth Mashroo <pmashroo58@gmail.com>
2020-01-14 22:37:34 +0530

    Error Logging and Error Page modified

--- MobileFinanceErp/App_Start/FilterConfig.cs
+++ MobileFinanceErp/App_Start/FilterConfig.cs
@@ -1,4 +1,5 @@
-﻿using System.Web;
+﻿using MobileFinanceErp.CustomFilters;
+using System.Web;
 using System.Web.Mvc;
 
 namespace MobileFinanceErp
@@ -8,6 +9,7 @@ namespace MobileFinanceErp
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            filters.Add(new LogCustomExceptionFilter());
         }
     }
 }
--- MobileFinanceErp/Controllers/BaseController.cs
+++ MobileFinanceErp/Controllers/BaseController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MobileFinanceErp.CustomFilters;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,6 +7,7 @@ using System.Web.Mvc;
 
 namespace MobileFinanceErp.Controllers
 {
+    [LogCustomExceptionFilter]
     public class BaseController : Controller
     {
         public void AddMessage(string message)
